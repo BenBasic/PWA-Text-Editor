13affa8c3acc1d350147535675848517cacb7ce3
BenBasic <benjaminmbasic@gmail.com>
2022-07-18 17:31:23 -0400

    Added scripts to package.json and implemented asset cashing in src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -27,4 +27,17 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // Makes a register route for caching the style, script, worker, image, document, and manifest files (request.destination) used for finding those files within the requested destination
+  ({ request }) => ['style', 'script', 'worker', 'image', 'document', 'manifest'].includes(request.destination), 
+  // StaleWhileRevalidate allows quick request response with a cached response if available, falling back to the network request if it's not cached. The network request is then used to update the cache
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      // CacheableResponsePlugin is used to only cache requests that result in a 0 or 200 status, preventing bad requests from being saved into the cache
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
